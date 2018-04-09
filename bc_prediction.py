886ae6f5da7f8f0d41d66080b93f6af8ef4efa8b
benfernandez <fernandezb@gwu.edu>
2018-04-09 12:18:14 -0400

    opened data vs target for half size

--- bc_prediction.py
+++ bc_prediction.py
@@ -6,19 +6,23 @@ bc = load_breast_cancer();
 clf = svm.SVC(gamma =0.001, C=100)
 
 print(bc.keys())
-print(len(bc.data))
-print(type(bc.data))
+print("Size: ",len(bc.data))
+print("Features: ",list(bc.feature_names))
+print("Target names: ",list(bc.target_names))
 
 bc_half = int(len(bc.data) / 2)
-print(bc_half)
+
 
 datax,targety = bc.data[:bc_half],bc.target[:bc_half]
+
+print(datax)
+print(targety)
+
 clf.fit(datax,targety)
 
 for x in range(1,11):
-    print("Prediction:    ",clf.predict(bc.data[[-x]]))
-    print("Actual target: ",bc.target[[-x]],"\n")
+    print("Prediction: ",clf.predict(bc.data[[-x]]))
+    print("Target:     ",bc.target[[-x]],"\n")
 
 
-print(list(bc.target_names))
 print("Accuracy: ",clf.score(bc.data,bc.target))
